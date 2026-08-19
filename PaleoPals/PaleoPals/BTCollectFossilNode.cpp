a55d0d86bb4e168c421609f50b28764edc343d93
JadFuhr <C00290965@itcarlow.ie>
2026-08-19 21:24:51 +0200

    fossil collect node now selects first available collectible and added debug statement

--- PaleoPals/PaleoPals/BTCollectFossilNode.cpp
+++ PaleoPals/PaleoPals/BTCollectFossilNode.cpp
@@ -8,5 +8,17 @@ BTStatus BTCollectFossilNode::tick(float dt)
 {
 	std::cout << "collect fossil ticked" << std::endl;
 
+	auto& fossils = m_map.getFossilManager().getAllCollectibles();
+
+	for (auto& c : fossils)
+	{
+		if (!c.isPickedUp)
+		{
+			std::cout << "looking for collectible" << c.collectibleIndex << " at tile: << " << c.gridRow << ", " << c.gridCol << "with value of: " << c.monetaryValue << std::endl;
+
+			return BTStatus::Running;
+		}
+	}
+
 	return BTStatus::Success;
 }
